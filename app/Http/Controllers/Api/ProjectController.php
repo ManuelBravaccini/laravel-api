d1658a274cedf6006a823b623d0f0d5f90bca380
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2023-03-02 23:09:41 +0100

    Implemented API show of projects

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -16,4 +16,12 @@ class ProjectController extends Controller
             'results' => $projects
         ]);
     }
+
+    public function show(Project $project)
+    {
+        return response()->json([
+            'success' => true,
+            'results' => $project
+        ]);
+    }
 }
